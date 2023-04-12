36624b45b2301da9835fa7a19eefcccd32e9d895
KolybinDmitry <113972408+KolybinDmitry@users.noreply.github.com>
2023-04-12 16:12:38 +0300

    f

---
+++ AudioPlayer/MainWindow.xaml.cs
@@ -0,0 +1,116 @@
+﻿using Microsoft.WindowsAPICodePack.Dialogs;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+
+namespace AudioPlayer
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        // изменить Position трэка при изменении значения слайдера мышкой 
+        // рандомайзер
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            PlayerManager.SetPlayerElements(mediaElement, labelFileInfo);
+            sliderVolume.Value = 0.5f;
+        }
+        private void rButtonNext_Click(object sender, RoutedEventArgs e)
+        {
+            PlayerManager.ShiftIndx(true);
+            PlayerManager.NewTrack();
+
+            var path = Path.Combine(Environment.CurrentDirectory, "UI", "play.png");
+            var uri = new Uri(path);
+            rButtonPlayPause.LargeImageSource = new BitmapImage(uri);
+        }
+        private void rButtonPrev_Click(object sender, RoutedEventArgs e)
+        {
+            PlayerManager.ShiftIndx(false);
+            PlayerManager.NewTrack();
+
+            var path = Path.Combine(Environment.CurrentDirectory, "UI", "play.png");
+            var uri = new Uri(path);
+            rButtonPlayPause.LargeImageSource = new BitmapImage(uri);
+        }
+        private void rButtonPlayPause_Click(object sender, RoutedEventArgs e)
+        {
+            PlayerManager.PlayPause();
+
+            var path = PlayerManager.Play
+                ? Path.Combine(Environment.CurrentDirectory, "UI", "pause.png")
+                : Path.Combine(Environment.CurrentDirectory, "UI", "play.png");
+
+            var uri = new Uri(path);
+            rButtonPlayPause.LargeImageSource = new BitmapImage(uri);
+        }
+        private void rButtonFolder_Click(object sender, RoutedEventArgs e)
+        {
+            string[] files = null;
+            CommonOpenFileDialog dialog = new CommonOpenFileDialog()
+            {
+                IsFolderPicker = true
+            };
+            CommonFileDialogResult result = dialog.ShowDialog();
+            if (result == CommonFileDialogResult.Ok)
+                files = Directory.GetFiles(dialog.FileName);
+
+            PlayerManager.NewTrackList(files);
+            PlayerManager.NewTrack();
+        }
+        private void rButtonMixType_Click(object sender, RoutedEventArgs e)
+        {
+            PlayerManager.NewMixType();
+
+            string path = "";
+
+            switch (PlayerManager.MixType)
+            {
+                case PlayerManager._MixType.overkill:
+                    path = Path.Combine(Environment.CurrentDirectory, "UI", "overkill.png");
+                    break;
+                case PlayerManager._MixType.loop:
+                    path = Path.Combine(Environment.CurrentDirectory, "UI", "loop.png");
+                    break;
+                case PlayerManager._MixType.random:
+                    path = Path.Combine(Environment.CurrentDirectory, "UI", "random.png");
+                    break;
+            }
+            var uri = new Uri(path);
+            rButtonMixType.LargeImageSource = new BitmapImage(uri);
+        }
+
+        private void mediaElement_LayoutUpdated(object sender, EventArgs e)
+        {
+            if (mediaElement.Source == null)
+                return;
+
+            labelTimeStart.Content = mediaElement.Position.ToString().Substring(0, 8);
+            sliderTrackLine.Value = mediaElement.Position.TotalSeconds;
+            labelTimeEnd.Content = TimeSpan.FromSeconds(sliderTrackLine.Maximum - mediaElement.Position.TotalSeconds).ToString().Substring(0, 8);
+        }
+
+        private void mediaElement_MediaOpened(object sender, RoutedEventArgs e)
+        {
+            if (mediaElement.Source == null)
+                return;
+            sliderTrackLine.Maximum = mediaElement.NaturalDuration.TimeSpan.TotalSeconds;
+        }
+
+        private void sliderVolume_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            mediaElement.Volume = sliderVolume.Value;
+        }
+    }
+}
---
+++ AudioPlayer/PlayerManager.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows;
+
+namespace AudioPlayer
+{
+    public static class PlayerManager
+    {
+        public enum _MixType
+        {
+            overkill,
+            loop,
+            random
+        }
+        public enum _WarningType
+        {
+            endList,
+            emptyList
+        }
+        const string defaultFileName = "None audio track";
+        public static List<string> MusicFiles { get; private set; } = new List<string>();
+        public static bool Play { get; private set; }
+        public static int Indx { get; private set; }
+        public static _MixType MixType { get; private set; }
+
+        private static MediaElement mediaElement;
+        private static Label labelFileInfo;
+        private static RandomList randomList = new RandomList(0);
+
+        public static void SetPlayerElements(MediaElement _mediaElement, Label _labelFileInfo)
+        {
+            mediaElement = _mediaElement ?? throw new ArgumentNullException(nameof(_mediaElement));
+            labelFileInfo = _labelFileInfo ?? throw new ArgumentNullException(nameof(_labelFileInfo));
+        }
+
+        private static void Warning(_WarningType warningType)
+        {
+            switch (warningType)
+            {
+                case _WarningType.emptyList:
+                    MessageBox.Show(
+                        "Аудиофайлы не найдены",
+                        "Warning!",
+                        MessageBoxButton.OK,
+                        MessageBoxImage.Warning
+                        );
+                    break;
+                case _WarningType.endList:
+                    MessageBox.Show(
+                        "Файлы кончились. Выберете новые",
+                        "Warning!",
+                        MessageBoxButton.OK,
+                        MessageBoxImage.Warning
+                        );
+                    break;
+            }
+        }
+
+        public static void NewTrackList(string[] files)
+        {
+            mediaElement.Source = null;
+            // Нужно ли иначе фильтровать файлы?
+            Regex regex = new Regex(@"(\w)*(.mp3|.wav|.flac|.aac)$");
+            MusicFiles = files != null
+                ? files.Where(it => regex.IsMatch(it)).ToList()
+                : new List<string>();
+
+            randomList = new RandomList(MusicFiles.Count);
+
+            if (MusicFiles.Count == 0)
+            {
+                Warning(_WarningType.emptyList);
+                return;
+            }
+            Indx = 0;
+        }
+
+        public static void NewTrack()
+        {
+            if (MusicFiles.Count == 0)
+            {
+                Warning(_WarningType.emptyList);
+                return;
+            }
+
+            if (Indx >= 0 && Indx < MusicFiles.Count)
+            {
+                mediaElement.Source = new Uri(MusicFiles[Indx]);
+                mediaElement.LoadedBehavior = MediaState.Pause;
+            }
+            else
+            {
+                Warning(_WarningType.endList);
+                return;
+            }
+
+            string fileName = mediaElement.Source != null
+                ? mediaElement.Source.ToString()
+                : defaultFileName;
+            labelFileInfo.Content = fileName.Substring(fileName.LastIndexOf("/") + 1);
+        }
+
+        public static void ShiftIndx(bool div)
+        {
+            switch (MixType)
+            {
+                case _MixType.overkill:
+                    if (Indx == MusicFiles.Count - 1 && div || Indx == 0 && !div)
+                    {
+                        Warning(_WarningType.endList);
+                        mediaElement.Source = null;
+                    }
+                    Indx = div ? ++Indx : --Indx;
+                    break;
+                case _MixType.loop:
+                    Indx = div
+                    ? (++Indx) % MusicFiles.Count
+                    : (MusicFiles.Count + --Indx) % MusicFiles.Count;
+                    break;
+                case _MixType.random:
+                    Indx = randomList.Indxs[randomList.GetNewIndx(div)];
+                    break;
+            }
+        }
+
+        public static void PlayPause()
+        {
+            if (MusicFiles.Count == 0)
+            {
+                Warning(_WarningType.emptyList);
+                return;
+            }
+            Play = !Play;
+            mediaElement.LoadedBehavior = Play
+                ? MediaState.Play
+                : MediaState.Pause;
+        }
+
+        public static void NewMixType()
+        {
+            MixType = (_MixType)(((int)MixType + 1) % 3);
+        }
+    }
+
+    class RandomList
+    {
+        public List<int> Indxs { get; private set; } = new List<int>();
+        private int ptr = 0;
+        private List<int> values = new List<int>();
+        public RandomList(int count)
+        {
+            for (int i = 0; i < count; ++i)
+                values.Add(i);
+            for (int i = 0; i < count; ++i)
+            {
+                int _value = values[new Random().Next(0, values.Count)];
+                Indxs.Add(_value);
+                values.Remove(_value);
+            }
+        }
+
+        public int GetNewIndx(bool div)
+        {
+            return div
+                ? (++ptr) % Indxs.Count
+                : (Indxs.Count + --ptr) % Indxs.Count;
+        }
+    }
+}
